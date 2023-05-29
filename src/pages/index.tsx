f62e02fdb7e277acf7f58bbc9c88f99c3c89fa99
Ayush Thapa <thapaaayush115@gmail.com>
2023-05-30 01:15:30 +0545

    feat: create basic navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
+import MainLayout from '@/components/layout/MainLayout';
 import Seo from '@/components/Seo';
-
-// import Vercel from '~/svg/Vercel.svg';
 
 export default function HomePage() {
   return (
-    <Layout>
+    <>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
 
       <main>ffffffffffffffffffffffffff</main>
-    </Layout>
+    </>
   );
 }
+
+HomePage.getLayout = function getLayout(page: React.ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
