308c6f9806ada9d44e1ed7b4f527b6b5178fd47d
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-06-03 14:33:29 +0545

    feat: changed popover to popper for page scrolling

--- src/features/dashboard/messages/index.tsx
+++ src/features/dashboard/messages/index.tsx
@@ -1,46 +1,53 @@
-import { IconButton, Popover } from '@mui/material';
+import { Box, ClickAwayListener, IconButton, Popper } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
 import { FiMessageCircle } from 'react-icons/fi';
 
 export default function MessagesPopover() {
-  const [panelPopover, setPanelPopover] = React.useState<HTMLElement | null>(
-    null
-  );
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setPanelPopover(event.currentTarget);
+  const [panelOpen, setPanelOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const toggleOpen = () => setPanelOpen(!panelOpen);
+  const handleClose = (event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target) &&
+      event.target !== anchorRef.current
+    ) {
+      return;
+    }
+
+    setPanelOpen(false);
   };
-  const handlePopoverClose = () => {
-    setPanelPopover(null);
-  };
+
   return (
-    <div>
-      <IconButton onClick={handlePopoverOpen}>
+    <div className='relative'>
+      <IconButton ref={anchorRef} onClick={toggleOpen}>
         <FiMessageCircle />
       </IconButton>
 
-      <Popover
-        anchorEl={panelPopover}
-        open={Boolean(panelPopover)}
-        onClose={handlePopoverClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <div className='flex h-[400px] w-[350px] flex-col overflow-hidden py-2'>
-          <div className='text-text-secondary-default px-2 text-xl font-medium'>
-            Messages
-          </div>
-          <div className='flex-1'></div>
-          <Link
-            href='/messages'
-            className='text-primary-main hover:text-primary-lightExtra px-2 text-center hover:underline'
+      <ClickAwayListener onClickAway={handleClose}>
+        <Popper
+          anchorEl={anchorRef.current}
+          open={panelOpen}
+          className='z-[100] rounded bg-white'
+        >
+          <Box
+            boxShadow={3}
+            className='flex h-[400px] w-[350px] flex-col overflow-hidden py-2'
           >
-            See all
-          </Link>
-        </div>
-      </Popover>
+            <div className='text-text-secondary-default px-2 text-xl font-medium'>
+              Messages
+            </div>
+            <div className='flex-1'></div>
+            <Link
+              href='/messages'
+              className='text-primary-main hover:text-primary-lightExtra px-2 text-center hover:underline'
+            >
+              See all
+            </Link>
+          </Box>
+        </Popper>
+      </ClickAwayListener>
     </div>
   );
 }
--- src/features/dashboard/notification/index.tsx
+++ src/features/dashboard/notification/index.tsx
@@ -1,38 +1,45 @@
-import { IconButton, Popover } from '@mui/material';
+import { Box, ClickAwayListener, IconButton, Popper } from '@mui/material';
 import React from 'react';
 import { RiNotification2Fill } from 'react-icons/ri';
 
 export default function NotificationsPopover() {
-  const [panelPopover, setPanelPopover] = React.useState<HTMLElement | null>(
-    null
-  );
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setPanelPopover(event.currentTarget);
+  const [panelOpen, setPanelOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const toggleOpen = () => setPanelOpen(!panelOpen);
+  const handleClose = (event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target) &&
+      event.target !== anchorRef.current
+    ) {
+      return;
+    }
+
+    setPanelOpen(false);
   };
-  const handlePopoverClose = () => {
-    setPanelPopover(null);
-  };
+
   return (
-    <div>
-      <IconButton onClick={handlePopoverOpen}>
+    <div className='relative'>
+      <IconButton ref={anchorRef} onClick={toggleOpen}>
         <RiNotification2Fill />
       </IconButton>
 
-      <Popover
-        anchorEl={panelPopover}
-        open={Boolean(panelPopover)}
-        onClose={handlePopoverClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <div className='flex h-[400px] w-[350px] flex-col overflow-hidden py-2'>
-          <div className='text-text-secondary-default px-2 text-xl font-medium'>
-            Notifications
-          </div>
-        </div>
-      </Popover>
+      <ClickAwayListener onClickAway={handleClose}>
+        <Popper
+          anchorEl={anchorRef.current}
+          open={panelOpen}
+          className='z-[100] rounded bg-white'
+        >
+          <Box
+            boxShadow={3}
+            className='flex h-[400px] w-[350px] flex-col overflow-hidden py-2'
+          >
+            <div className='text-text-secondary-default px-2 text-xl font-medium'>
+              Notifications
+            </div>
+          </Box>
+        </Popper>
+      </ClickAwayListener>
     </div>
   );
 }
