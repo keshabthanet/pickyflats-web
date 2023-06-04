ea2f49d49fb2dce22ce96e1eb8a7608e97dcd626
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-06-04 09:00:17 +0545

    feat: type support for user

---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export type User = {
+  $id: string;
+  $createdAt: string;
+  $updatedAt: string;
+  name: string;
+  registration: string;
+  status: boolean;
+  passwordUpdate: string;
+  email: string;
+  phone: string;
+  emailVerification: boolean;
+  phoneVerification: boolean;
+  prefs: [key: string];
+};
