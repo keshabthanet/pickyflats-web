12efcb42e0ec0b0acb818c3c8b5af179a58bc99e
Ayush Thapa <thapaaayush115@gmail.com>
2023-06-13 00:44:17 +0545

    feat: create summary card in dashboard

---
+++ src/pageComponents/dashboard/cards/SummaryCards.tsx
@@ -0,0 +1,17 @@
+interface Iprop {
+  title: string;
+  count: number;
+  subtitle?: string;
+}
+
+export const SummaryCard = (props: Iprop) => {
+  return (
+    <div>
+      <div className='h-[120px] w-[300px] rounded-lg bg-slate-200 p-5 text-center shadow-lg'>
+        <h2 className=' text-primary-main text-3xl font-bold'>{props.count}</h2>
+        <h3 className='  font-semibold text-black'>{props.title}</h3>
+        <h3 className='  text-sm'>{props.subtitle}</h3>
+      </div>
+    </div>
+  );
+};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle, Button, IconButton } from '@mui/material';
 import React from 'react';
 
 import DashboardLayout from '@/components/layout/DashboardLayout';
@@ -5,11 +6,43 @@ import DashboardLayout from '@/components/layout/DashboardLayout';
 import useAuthStore from '@/store/useAuthStore';
 
 import withAuth, { WithAuthProps } from '@/hoc/withAuth';
+import { SummaryCard } from '@/pageComponents/dashboard/cards/SummaryCards';
+import { IoInformationCircle, IoInformationOutline } from 'react-icons/io5';
 
 export default function DashboardPage() {
   const { user } = useAuthStore();
   console.log(user);
-  return <div>DashboardPage</div>;
+  return (
+    <div className='relative h-full w-full overflow-y-scroll p-5'>
+      <div className=' '>
+        <Alert severity='info' className='kdj'>
+          <div className='items-start space-y-2 md:flex md:justify-between md:space-x-2'>
+            <div>
+              <AlertTitle>Profile Verification Required</AlertTitle>
+              Create your listing now, but please note that it will be visible
+              to the public only after profile verification.
+            </div>
+            <div className='my-auto flex md:justify-end'>
+              <Button variant='contained'>Start Verification</Button>
+            </div>
+          </div>
+        </Alert>
+      </div>
+      <div className='flex gap-9 py-5'>
+        <SummaryCard title='Listed Flats' count={3} subtitle=' ' />
+        <SummaryCard
+          title='Total Comments'
+          count={33}
+          subtitle='31 comments Last Month'
+        />
+      </div>
+      <div className='absolute bottom-5 right-5 flex h-[50px] w-[50px] flex-col justify-center rounded-full border-[2px] bg-slate-200 text-center align-middle'>
+        <IconButton>
+          <IoInformationOutline />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
 function LayoutWrapper(props: WithAuthProps) {
