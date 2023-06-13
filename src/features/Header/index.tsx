b9570f030409ab0c5b4d9e0868806b4fbc3a606d
Ayush Thapa <thapaaayush115@gmail.com>
2023-06-13 23:36:19 +0545

    fix: move queries to global store

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -1,7 +1,10 @@
 import InputBase from '@mui/material/InputBase';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { MdOutlineTune } from 'react-icons/md';
+
+import useListingsStore from '@/store/useListingsStore';
 
 import FlatTypeSelectorOptions from '@/features/home/FlatTypeSelectorOptions';
 
@@ -9,21 +12,39 @@ import FilterModal from '../Filter';
 
 export const Header = () => {
   const [openModal, setOpenModal] = useState(false);
+
+  const { query, minPrice, maxPrice, purpose, bedrooms, bathrooms, kitchen } =
+    useListingsStore();
+
+  const { setQuery } = useListingsStore();
+
+  const router = useRouter();
+  const handleSearch = (KeyCode: string) => {
+    if (KeyCode == 'Enter') {
+      router.push(
+        `/?query=${query}&min=${minPrice}&max=${maxPrice}&purpose=${purpose}&bedRoom=${bedrooms}&bathRoom=${bathrooms}&kitchen=${kitchen}`
+      );
+    }
+  };
   // comment
   return (
     <div className=' flex h-[85vh] w-full flex-col text-center'>
-      {/* <div>top filters</div> */}
       <div className=' flex flex-grow flex-col justify-center align-middle'></div>
       <div className='h-[100px] flex-shrink'>
         <div className=' m-auto flex h-[50px] w-[80%] gap-2 md:w-[50%] '>
+          {/* <form className=' m-auto flex h-[50px] w-[80%] gap-2 md:w-[50%] '> */}
           <InputBase
             type='text'
-            placeholder='What is in your mind?'
+            placeholder='Search Your dream'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             startAdornment={
               <FiSearch className='text-primary-main mx-3 cursor-pointer text-[25px] ' />
             }
+            onKeyDown={(e) => handleSearch(e.key)}
             className='hover:border-primary-main h-[50px] w-full  rounded-[25px] border-[2px] text-[18px] text-white '
           />
+          {/* </form> */}
           <div
             className=' flex cursor-pointer items-center rounded-2xl border-2 border-white px-2 py-1'
             onClick={() => setOpenModal(true)}
--- src/store/useListingsStore.tsx
+++ src/store/useListingsStore.tsx
@@ -2,6 +2,7 @@ import { createSelectorHooks } from 'auto-zustand-selectors-hook';
 import create from 'zustand';
 
 import { Listing } from '@/types/listing';
+import { useRouter } from 'next/router';
 
 interface ActiveFilter {
   label: string;
@@ -15,15 +16,23 @@ interface ListingsStore {
 
   extraFilterActive: boolean;
   // filter data
+  query: string;
+  purpose: 'rent' | 'sell';
   minPrice: number;
   maxPrice: number;
   bedrooms: number;
   bathrooms: number;
+  kitchen: number;
 
+  setQuery: (type: string) => void;
+
+  setPurpose: (type: 'sell' | 'rent') => void;
   setMinPrice: (type: number) => void;
   setMaxPrice: (type: number) => void;
   setBedrooms: (type: number) => void;
   setBathrooms: (type: number) => void;
+  setKitchen: (type: number) => void;
+
   setExtraFiilterActive: (active: boolean) => void;
 
   //
@@ -31,32 +40,47 @@ interface ListingsStore {
   refresh: () => void;
 }
 
-const useListingsStoreBase = create<ListingsStore>((set) => ({
-  listings: [],
-  // filter
-  minPrice: 0,
-  maxPrice: 0,
-  bedrooms: 0,
-  bathrooms: 0,
-  activeTypeFilter: null,
-  setListings: (listings) => set(() => ({ listings })),
-  setTypeFilter: (type) => set(() => ({ activeTypeFilter: type })),
+const useListingsStoreBase = create<ListingsStore>((set) => {
+  // const router=useRouter()
+  return {
+    listings: [],
+    // filter
+    query: String(''),
 
-  // filter
-  setMinPrice: (price) => set(() => ({ minPrice: price })),
-  setMaxPrice: (price) => set(() => ({ maxPrice: price })),
-  setBedrooms: (room) => set(() => ({ bedrooms: room })),
-  setBathrooms: (room) => set(() => ({ bathrooms: room })),
-  extraFilterActive: false,
-  setExtraFiilterActive: (active) => set(() => ({ extraFilterActive: active })),
+    minPrice: 0,
+    maxPrice: 0,
+    bedrooms: 0,
+    bathrooms: 0,
+    kitchen: 0,
+    purpose: 'rent',
 
-  //
+    activeTypeFilter: null,
+    setListings: (listings) => set(() => ({ listings })),
+    setTypeFilter: (type) => set(() => ({ activeTypeFilter: type })),
 
-  refreshCount: 0,
-  refresh: () => {
-    set((state) => ({ refreshCount: state.refreshCount + 1 }));
-  },
-}));
+    // filter
+    setQuery: (query) => set(() => ({ query: query })),
+
+    setPurpose: (purpose) => set(() => ({ purpose: purpose })),
+
+    setMinPrice: (price) => set(() => ({ minPrice: price })),
+    setMaxPrice: (price) => set(() => ({ maxPrice: price })),
+    setBedrooms: (room) => set(() => ({ bedrooms: room })),
+    setBathrooms: (room) => set(() => ({ bathrooms: room })),
+    setKitchen: (kitchen) => set(() => ({ kitchen: kitchen })),
+
+    extraFilterActive: false,
+    setExtraFiilterActive: (active) =>
+      set(() => ({ extraFilterActive: active })),
+
+    //
+
+    refreshCount: 0,
+    refresh: () => {
+      set((state) => ({ refreshCount: state.refreshCount + 1 }));
+    },
+  };
+});
 
 const useListingsStore = createSelectorHooks(useListingsStoreBase);
 
